57278c0f9d37ea53a463bd628c80970de370739e
ronbarrantes <ronbarrantes@gmail.com>
2023-08-02 11:39:47 -0700

    Add some stuff to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-import Image from 'next/image'
+// import Image from 'next/image'
+
 import { UserButton } from '@clerk/nextjs'
 
-export default function Home() {
+// import { users } from '@/db/schema'
+import { db, UsersTable } from '../db/drizzle'
+
+export default async function Home() {
+  // const db = drizzle(sql)
+
+  // const allUsers = await db.select().from(UsersTable)
+  // const allUsers = await db.select().from(users)
+
+  // const client = postgres(connectionString)
+  // const db = drizzle(client)
+
+  // const allUsers = await db.select().from(users)
+
+  // console.log(allUsers)
+
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <div>
