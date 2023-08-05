c9acf06df03eaa0e7e6e0ea4f7dfb03da8f51d13
ronbarrantes <ronbarrantes@gmail.com>
2023-08-05 16:48:23 -0700

    Add init component

---
+++ src/app/components/Init.tsx
@@ -0,0 +1,6 @@
+// This init component is probably not the best idea but it is what I'm going
+// to do for the time being until I figure out a better way to use drizzle.
+
+const Init = () => {
+  return <></>
+}
