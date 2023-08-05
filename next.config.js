93b0e5b9705a4715498162b8ad0ee2aca008b8b6
ronbarrantes <ronbarrantes@gmail.com>
2023-08-05 16:46:38 -0700

    Add note for next config

--- next.config.js
+++ next.config.js
@@ -10,6 +10,18 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   swcMinify: true,
+
+  // refer to: https://nextjs.org/docs/app/building-your-application/optimizing/images
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 's3.amazonaws.com',
+  //       port: '',
+  //       pathname: '/my-bucket/**',
+  //     },
+  //   ],
+  // },
 }
 
 module.exports = nextConfig
