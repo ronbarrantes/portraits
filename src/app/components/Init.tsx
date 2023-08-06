9b2037fb19afa3f1ec35e68704a61e77bb5cd3b9
ronbarrantes <ronbarrantes@gmail.com>
2023-08-05 21:25:17 -0700

    Create the dumb init function

--- src/app/components/Init.tsx
+++ src/app/components/Init.tsx
@@ -1,6 +1,24 @@
+import { init } from '@/db/init'
+import { db, ImageTable } from '@/db/schema'
+
 // This init component is probably not the best idea but it is what I'm going
 // to do for the time being until I figure out a better way to use drizzle.
 
-const Init = () => {
+export const Init = async () => {
+  try {
+    await db.select().from(ImageTable).limit(1)
+  } catch (e: any) {
+    console.log('ERROR:', e.message)
+    if (e.message === `relation "images" does not exist`) {
+      console.log(
+        'Table does not exist, creating and seeding it with dummy data now...',
+      )
+      await init()
+      await db.select().from(ImageTable).limit(1)
+    } else {
+      throw e
+    }
+  }
+
   return <></>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,23 +10,6 @@ import { addImageToDB } from './actions/image-upload'
 // MAKE A INIT COMPONENT
 
 export default async function Home() {
-  let images
-  // let startTime = Date.now()
-  try {
-    images = await db.select().from(ImageTable).limit(1)
-  } catch (e: any) {
-    console.log('ERROR ------>>>>>', e.message)
-    if (e.message === `relation "images" does not exist`) {
-      console.log(
-        'Table does not exist, creating and seeding it with dummy data now...',
-      )
-      await init()
-      images = await db.select().from(ImageTable)
-    } else {
-      throw e
-    }
-  }
-
   return (
     <PageLayout>
       <h1>Hello world</h1>
