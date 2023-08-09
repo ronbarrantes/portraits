fb9f8a7498a97d56a47d1e4b7f46e18e74bf2255
ronbarrantes <ronbarrantes@gmail.com>
2023-08-09 11:32:43 -0700

    Improve nav

--- src/app/components/layouts.tsx
+++ src/app/components/layouts.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 
+import { UserButton } from '@clerk/nextjs'
+
 import { Logo } from '@/ui/Logo'
 import { DashboardNav, MainNav } from './navigation'
 
@@ -14,7 +16,16 @@ export const PageLayout = ({ children }: PageLayoutProps) => {
         <Link href="/" aria-label="Portrait app logo">
           <Logo />
         </Link>
-        <MainNav />
+        <MainNav>
+          <UserButton
+            appearance={{
+              elements: {
+                avatarBox: 'w-9 h-9', // THIS IS HOW TO CONTROL THE SIZE
+              },
+            }}
+            afterSignOutUrl="/"
+          />
+        </MainNav>
       </header>
       <main className="flex flex-col items-center justify-between w-full gap-5 p-1 sm:max-w-6xl">
         {children}
--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -2,7 +2,13 @@ import Link from 'next/link'
 
 import { UserButton } from '@clerk/nextjs'
 
-const LinkButton = ({ href, children }: { href: string; children: string }) => (
+const LinkButton = ({
+  href,
+  children,
+}: {
+  href: string
+  children: React.ReactNode
+}) => (
   <Link
     href={href}
     className="pb-0.5 hover:border-b-2 border-transparent hover:border-violet-500 hover:text-violet-500 disabled:!bg-rose-900"
@@ -12,45 +18,59 @@ const LinkButton = ({ href, children }: { href: string; children: string }) => (
   </Link>
 )
 
-export const MainNav = () => {
+type NavItem = {
+  href: string
+  name: string | React.ReactNode
+}
+interface NavProps {
+  navItems: NavItem[]
+  children?: React.ReactNode
+}
+
+export const mainNavItems: NavItem[] = [
+  {
+    href: '/',
+    name: 'main',
+  },
+  {
+    href: '/dashboard',
+    name: 'dashboard',
+  },
+  {
+    href: '/generate',
+    name: 'generate',
+  },
+]
+
+export const dashboardNavItems: NavItem[] = [
+  {
+    href: '/dashboard',
+    name: 'uploaded',
+  },
+  {
+    href: '/dashboard/generated',
+    name: 'generate',
+  },
+]
+
+export const Nav = ({ navItems, children }: NavProps) => {
   return (
     <nav className="flex items-center">
       <ul className="flex items-center gap-3">
-        <li>
-          <LinkButton href="/">main</LinkButton>
-        </li>
-        <li>
-          <LinkButton href="/dashboard">dashboard</LinkButton>
-        </li>
-        <li>
-          <LinkButton href="/generate">generate</LinkButton>
-        </li>
+        {navItems.map((item, idx) => (
+          <li key={idx}>
+            <LinkButton href={item.href}>{item.name}</LinkButton>
+          </li>
+        ))}
       </ul>
-      <div className="ml-3 w-9 h-9">
-        <UserButton
-          appearance={{
-            elements: {
-              avatarBox: 'w-9 h-9', // THIS IS HOW TO CONTROL THE SIZE
-            },
-          }}
-          afterSignOutUrl="/"
-        />
-      </div>
+
+      {children && <div className="ml-3 w-9 h-9">{children}</div>}
     </nav>
   )
 }
 
-export const DashboardNav = () => {
-  return (
-    <nav className="flex items-center">
-      <ul className="flex items-center gap-3">
-        <li>
-          <LinkButton href="/dashboard">uploaded</LinkButton>
-        </li>
-        <li>
-          <LinkButton href="/dashboard/generated">generate</LinkButton>
-        </li>
-      </ul>
-    </nav>
-  )
-}
+export const MainNav = ({ children }: { children: React.ReactNode }) => (
+  <Nav navItems={mainNavItems}>{children}</Nav>
+)
+
+export const DashboardNav = () => <Nav navItems={dashboardNavItems} />
