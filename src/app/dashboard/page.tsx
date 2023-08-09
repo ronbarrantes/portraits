729fe8d0a6154870e1d501e8618e154fe5ddd047
ronbarrantes <ronbarrantes@gmail.com>
2023-08-09 12:14:12 -0700

    Add image upload to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,23 @@
 // MAKE A INIT COMPONENT
+import { ImageUpload } from '@components/ImageUpload'
 
 export default async function Dashboard() {
   // an array just to show images
   const images = ['1', '2', '3', '4', '5', '1', '2', '3', '4', '5', '1', '2']
 
   return (
-    <ul className="flex flex-wrap w-full h-full">
-      {images.map((image, idx) => (
-        <li
-          className="flex items-center justify-center w-1/2 h-fit sm:w-1/4 md:w-1/6"
-          key={idx}
-        >
-          <div className="flex w-40 h-40 m-2 bg-red-500">{}</div>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ImageUpload />
+      <ul className="flex flex-wrap w-full h-full">
+        {images.map((image, idx) => (
+          <li
+            className="flex items-center justify-center w-1/2 h-fit sm:w-1/4 md:w-1/6"
+            key={idx}
+          >
+            <div className="flex w-40 h-40 m-2 bg-red-500">{}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
