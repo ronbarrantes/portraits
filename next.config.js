dffba0847329fc854b7215dadda9e280e0df9301
ronbarrantes <ronbarrantes@gmail.com>
2023-08-09 16:42:42 -0700

    Configure next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+// const dotenv = require('dotenv')
+
+// dotenv.config({
+//   path: '.env.development.local',
+// })
+
 const nextConfig = {
   experimental: {
     serverActions: true,
@@ -12,16 +18,16 @@ const nextConfig = {
   swcMinify: true,
 
   // refer to: https://nextjs.org/docs/app/building-your-application/optimizing/images
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 's3.amazonaws.com',
-  //       port: '',
-  //       pathname: '/my-bucket/**',
-  //     },
-  //   ],
-  // },
+  images: {
+    // domains: [`${process.env.AWS_S3_BUCKET_NAME}.s3.amazonaws.com`],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: `**.s3.amazonaws.com`,
+        // pathname: '/**/*',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
