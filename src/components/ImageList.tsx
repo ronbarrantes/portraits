ce1fd0ef0d0119689a8febeafe615d1d5f3ce6a9
ronbarrantes <ronbarrantes@gmail.com>
2023-08-14 12:30:24 -0700

    Cleanup

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,18 +1,19 @@
 'use client'
-import { AddImageToDB } from '@/app/actions/images'
+// import { AddImageToDB } from '@actions/images'
+
 import { Image } from '@/db/schema'
 
 interface ImageListProps {
   images: Image[]
-  addImageToDB: AddImageToDB
+  // addImageToDB: AddImageToDB
 }
 
-export const ImageList = async ({ images, addImageToDB }: ImageListProps) => {
+export const ImageList = async ({
+  images,
+} // addImageToDB
+
+: ImageListProps) => {
   console.log('IMAGES', images)
 
-  return (
-    <>
-      <button onClick={() => addImageToDB()}>Add To DB</button>
-    </>
-  )
+  return <>{/* <button onClick={() => addImageToDB()}>Add To DB</button> */}</>
 }
